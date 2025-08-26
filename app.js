06818a386c30ec4d665a03a8ceb206f8c045648c
othman <oteman.raja@gmail.com>
2025-08-27 00:23:14 +0100

    fix error

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ import contactRouter from "./routes/contact.routes.js";
 import paylinkRouter from "./routes/paylink.routes.js";
 const app = express();
 
+app.set("trust proxy", 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
