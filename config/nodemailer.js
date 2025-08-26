c04dc1d4ad4860b4678d267eff2a9cf827910aa5
othman <oteman.raja@gmail.com>
2025-08-27 00:31:49 +0100

    nodemail

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -3,10 +3,12 @@ import nodemailer from "nodemailer";
 import { Email_PASSWORD, EMAIL_COMPANY_FROM } from "./env.js";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // use SSL
   auth: {
-    user: EMAIL_COMPANY_FROM,
-    pass: Email_PASSWORD,
+    user: EMAIL_COMPANY_FROM, // e.g. "yourcompany@gmail.com"
+    pass: Email_PASSWORD,     // Google App Password (NOT normal Gmail password)
   },
 });
 
